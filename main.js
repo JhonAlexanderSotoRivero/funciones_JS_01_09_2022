ff98915b2e232dd38fdf059720478d88d7c0dd27
JhonAlexanderSotoRivero <jhonsoto822@gmail.com>
2022-09-03 02:50:25 -0500

    Fórmula para hallar el área de un Triángulo

--- main.js
+++ main.js
@@ -1,10 +1,12 @@
 
 
-let b = Number(prompt("Ingrese la base del Rectangulo", 1));
-let h = Number(prompt("Ingrese la altura del Rectangulo", 1));
-function formulaAreaRectangulo(base, altura, imagen="imagenes/Rectangulo.png"){
+let b = Number(prompt("Ingrese la base del Triangulo", 1));
+let h = Number(prompt("Ingrese la altura del Triangulo", 1));
+let area;
+    area = (b * h) / 2;
+function formulaAreaTriangulo(area, imagen="imagenes/Triangulo.png"){
     document.body.insertAdjacentHTML("afterbegin", `<img src="${imagen}">`);
-    return base * altura;
+    return area;
 }
-console.log(`El Area del Rectangulo es: ${formulaAreaRectangulo(b, h)}`);
+console.log(`El Area del Triangulo es: ${formulaAreaTriangulo(area)}`);
 
