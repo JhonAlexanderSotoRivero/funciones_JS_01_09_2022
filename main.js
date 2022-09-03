c85db9d674e08e8aac30955e72ed897cfad44d6d
JhonAlexanderSotoRivero <jhonsoto822@gmail.com>
2022-09-03 04:11:21 -0500

    Fórmula para hallar el volumen de un Ortoedro

--- main.js
+++ main.js
@@ -1,11 +1,13 @@
 
 
-let a = Number(prompt("Ingrese la arista del Cubo", 1));
+let l = Number(prompt("Ingrese lel largo del Ortoedro", 1));
+let a = Number(prompt("Ingrese la ancho del Ortoedro", 1));
+let h = Number(prompt("Ingrese la altura del Ortoedro", 1));
 let Volumen;
-    Volumen =  Math.pow(a,3);
-function formulaVolumenCubo(area, imagen="imagenesVol/Cubo.png"){
+    Volumen =  (l * a * h);
+function formulaVolumenOrtoedro(area, imagen="imagenesVol/Ortoedro.png"){
     document.body.insertAdjacentHTML("afterbegin", `<img src="${imagen}">`);
     return Volumen;
 }
-console.log(`El Volumen del Cubo es: ${formulaVolumenCubo(Volumen)}`);
+console.log(`El Volumen del Ortoedro es: ${formulaVolumenOrtoedro(Volumen)}`);
 
