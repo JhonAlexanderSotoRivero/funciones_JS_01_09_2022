87eb1b359087b239992ba4c346b7a26f91ee245b
JhonAlexanderSotoRivero <jhonsoto822@gmail.com>
2022-09-03 14:04:07 -0500

    Fórmula para hallar el volumen de un Cilindro

--- main.js
+++ main.js
@@ -1,12 +1,12 @@
 
 
-let Ab= Number(prompt("Ingrese el Area de la base del Prisma", 1));
-let h = Number(prompt("Ingrese la altura del Prisma", 1));
+let pi = Math.PI;
+let r = Number(prompt("Ingrese el radio del Cilindro", 1));
+let h = Number(prompt("Ingrese la altura del Cilindro", 1));
 let Volumen;
-    Volumen =  (Ab * h);
-function formulaVolumenPrisma(area, imagen="imagenesVol/Prisma.png"){
+    Volumen = pi * Math.pow(r,2) * h;
+function formulaVolumenCilindro(Volumen, imagen="imagenesVol/Cilindro.png"){
     document.body.insertAdjacentHTML("afterbegin", `<img src="${imagen}">`);
     return Volumen;
 }
-console.log(`El Volumen del Prisma es: ${formulaVolumenPrisma(Volumen)}`);
-
+console.log(`El Volumen del Cilindro es: ${formulaVolumenCilindro(Volumen)}`);
