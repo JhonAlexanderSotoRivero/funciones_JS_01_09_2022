04e5a47a0edf8e51c76ab2ac5f15bbbbaf936441
JhonAlexanderSotoRivero <jhonsoto822@gmail.com>
2022-09-03 14:04:37 -0500

    Fórmula para hallar el volumen de un Cono

--- main.js
+++ main.js
@@ -1,12 +1,11 @@
 
 
-let pi = Math.PI;
-let r = Number(prompt("Ingrese el radio del Cilindro", 1));
-let h = Number(prompt("Ingrese la altura del Cilindro", 1));
+let Ab = Number(prompt("Ingrese el Area de base del Cono", 1));
+let h = Number(prompt("Ingrese la altura del Cono", 1));
 let Volumen;
-    Volumen = pi * Math.pow(r,2) * h;
-function formulaVolumenCilindro(Volumen, imagen="imagenesVol/Cilindro.png"){
+    Volumen = ((1/3 * Ab) * h);
+function formulaVolumenCono(Volumen, imagen="imagenesVol/Cono.png"){
     document.body.insertAdjacentHTML("afterbegin", `<img src="${imagen}">`);
     return Volumen;
 }
-console.log(`El Volumen del Cilindro es: ${formulaVolumenCilindro(Volumen)}`);
+console.log(`El Volumen del Cono es: ${formulaVolumenCono(Volumen)}`);
