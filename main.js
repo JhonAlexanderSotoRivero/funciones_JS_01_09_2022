b89d476e267a42f6ac7482a755fc399942e200fa
JhonAlexanderSotoRivero <jhonsoto822@gmail.com>
2022-09-03 03:22:32 -0500

    Fórmula para hallar el área de un Trapecio

--- main.js
+++ main.js
@@ -1,12 +1,13 @@
 
 
-let D = Number(prompt("Ingrese el diagonal mayor del Rombo", 1));
-let d = Number(prompt("Ingrese el diagonal menor del Rombo", 1));
+let B = Number(prompt("Ingrese la base mayor del Trapecio", 1));
+let b = Number(prompt("Ingrese la base menor del Trapecio", 1));
+let h= Number(prompt("Ingrese la altura del Trapecio", 1));
 let area;
-    area = (D * d) / 2;
-function formulaAreaRombo(area, imagen="imagenes/Rombo.png"){
+    area = ((B + b)/2) * h;
+function formulaAreaTrapecio(area, imagen="imagenes/Trapecio.png"){
     document.body.insertAdjacentHTML("afterbegin", `<img src="${imagen}">`);
     return area;
 }
-console.log(`El Area del Rombo es: ${formulaAreaRombo(area)}`);
+console.log(`El Area del Trapecio es: ${formulaAreaTrapecio(area)}`);
 
