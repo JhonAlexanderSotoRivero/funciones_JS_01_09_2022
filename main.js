defebd81ac00dc791adec9567058a9d63932b10b
JhonAlexanderSotoRivero <jhonsoto822@gmail.com>
2022-09-03 03:44:50 -0500

    Fórmula para hallar el área de un Circulo

--- main.js
+++ main.js
@@ -1,12 +1,12 @@
 
 
-let P = Number(prompt("Ingrese el perimetro del PoligonoRegular", 1));
-let a = Number(prompt("Ingrese el apotema del PoligonoRegular", 1));
+let pi = Math.PI;
+let r = Number(prompt("Ingrese el radio del Circulo", 1));
 let area;
-    area = ((P / 2) * a);
-function formulaAreaPoligonoRegular(area, imagen="imagenes/PoligonoRegular.png"){
+    area = (pi * Math.pow(r,2));
+function formulaAreaCirculo(area, imagen="imagenes/Circulo.png"){
     document.body.insertAdjacentHTML("afterbegin", `<img src="${imagen}">`);
     return area;
 }
-console.log(`El Area del PoligonoRegular es: ${formulaAreaPoligonoRegular(area)}`);
+console.log(`El Area del Circulo es: ${formulaAreaCirculo(area)}`);
 
